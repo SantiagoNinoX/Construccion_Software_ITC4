3891c102f656d5bc5baa241cc87d032fc4e3c3f6
Santiago Ramirez Niño <A01665906@tec.mx>
2025-02-18 00:12:45 -0600

    Animaciones listas mientras mamá llora

--- learning_bootstrap/index.js
+++ learning_bootstrap/index.js
@@ -1,11 +1,64 @@
 $(document).ready(function(){
-    $("#gatoid").click(function(){    //click, o mouseenter
-        $("#gatoid").hide();       //puedo darla animate({right:'250px'})
-    })
-    $("#gatoid").mouseleave(function(){    
-        $("#gatoid").fadeOut(2000);
-    })
-    $("#gatoid").mouseenter(function(){    
-        $("#gatoid").fadeIn(2000);
-    })
+    $("#infoNacho").hide();
+    $("#infoGabo").hide();
+    $("#infoOmar").hide();
+    $("#listaMenu").hide();
+    $("#fotoNacho").dblclick(function(){    
+        $("#infoNacho").fadeIn(200);       
+    });
+    $("#fotoGabo").dblclick(function(){    
+        $("#infoGabo").fadeIn(200);       
+    });
+    $("#fotoOmar").dblclick(function(){    
+        $("#infoOmar").fadeIn(200);       
+    });
+    $("#maps").mouseleave(function(){
+        $("#maps").hide();
+    });
+    $("#botonSlide").click(function(){
+        $("#listaMenu").slideDown();
+    });
 });
+
+function menus(dia){
+    if (dia === "lunes"){
+       console.log(`Mostrando menu del dia ${dia}`);
+       document.getElementById("infomenu").innerText="Sopa de letras, nachos con queso, carne al pastor, agua de limón y un cheesecake de frambuesa.";
+    }
+    if (dia === "martes"){
+       console.log(`Mostrando menu del dia ${dia}`);
+       document.getElementById("infomenu").innerText="Arroz rojo, tostadas preparadas, pechugas rellenas, agua de jamaica y gelatina de rompope.";
+    }
+    if (dia === "miercoles"){
+       console.log(`Mostrando menu del dia ${dia}`);
+       document.getElementById("infomenu").innerText="Consome de pollo, arroz blanco, enchiladas suizas, agua de horchata y arroz con leche.";
+    }
+    if (dia === "jueves"){
+       console.log(`Mostrando menu del dia ${dia}`);
+       document.getElementById("infomenu").innerText="Ensalada rusa, pozole rojo, picadillo de res, agua de fresa y pastel de chocolate.";
+    }
+    if (dia === "viernes"){
+       console.log(`Mostrando menu del dia ${dia}`);
+       document.getElementById("infomenu").innerText="Pasta boloñesa, pan de ajo, pizza de peperoni, sodas italianas y tiramisú.";
+    }
+    if (dia === "sabado"){
+       console.log(`Mostrando menu del dia ${dia}`);
+       document.getElementById("infomenu").innerText="Arroz blanco, camarones, tacos dorados, agua de rompope y pay de limon.";
+    }
+  }
+
+  function gatitoAscii(){
+    const gato = [
+       [' ', '/', '\\', '_', '/', '\\', ' '],
+       ['(', ' ', 'o', '.', 'o', ' ', ')'],
+       [' ', '>', ' ', '^', ' ', '<', ' ']
+   ];
+   
+   gato.forEach(fila => {
+       let linea = '';
+       fila.forEach(caracter => {
+           linea += caracter;
+       });
+       console.log(linea);
+   });
+  }
