d7f59a161a5d1ed90153b92a49c949871bf8506f
Santiago Ramirez Niño <A01665906@tec.mx>
2025-02-24 00:27:59 -0600

    Listo el backend. Canchola quiere regresar con Dariana

--- learning_bootstrap/backend_catcafe.py
+++ learning_bootstrap/backend_catcafe.py
@@ -21,3 +21,35 @@ def saludo():
 @app.route("/cambiaNacho")
 def nuevoNacho():
     return "https://images.unsplash.com/photo-1606208427954-aa8319c4815e?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2F0byUyMGJsYW5jb3xlbnwwfHwwfHx8MA%3D%3D"
+
+@app.route("/cambiaGabo")
+def nuevoGabo():
+    return "https://www.mirringo.com.co/Portals/mirringo/Images/articulos-actualidad-gatuna/5-mitos-sobre-los-gatos-naranjas/gatos-de-color-naranja.png?ver=2024-10-30-115216-760"
+
+@app.route("/cambiaOmar")
+def nuevoOmar():
+    return "https://www.hola.com/horizon/square/7a62013a79a9-por-que-gatos-negros-dan-mala-suerte-t.jpg"
+
+@app.route("/menuLunes")
+def menuMonday():
+    return "Sopa de letras, nachos con queso, carne al pastor, agua de limón y un cheesecake de frambuesa."
+
+@app.route("/menuMartes")
+def menuTuesday():
+    return "Arroz rojo, tostadas preparadas, pechugas rellenas, agua de jamaica y gelatina de rompope."
+
+@app.route("/menuMiercoles")
+def menuWednesday():
+    return "Consome de pollo, arroz blanco, enchiladas suizas, agua de horchata y arroz con leche."
+
+@app.route("/menuJueves")
+def menuThursday():
+    return "Ensalada rusa, pozole rojo, picadillo de res, agua de fresa y pastel de chocolate."
+
+@app.route("/menuViernes")
+def menuFriday():
+    return "Pasta boloñesa, pan de ajo, pizza de peperoni, sodas italianas y tiramisú."
+
+@app.route("/menuSabado")
+def menuSaturday():
+    return "Arroz blanco, camarones, tacos dorados, agua de rompope y pay de limon."
--- learning_bootstrap/index.js
+++ learning_bootstrap/index.js
@@ -24,6 +24,9 @@ $(document).ready(function(){
             alert("Acaba de cargar el menu. Status: " + status);
         });
     });
+    $("#hideMenu").click(function(){   //Ya no me deja volver a mostrar.
+        $("#resultadomenu").hide();
+    })
     $("#botonNacho").click(function(){
         $.get("http://127.0.0.1:5000/cambiaNacho", function(data, status){
             $("#fotoNacho").attr("src", data);
@@ -32,34 +35,60 @@ $(document).ready(function(){
             alert("Acaba de cambiar la foto de Nacho. Status: " + status);
         });
     });
-});
+    $("#botonGabo").click(function(){
+        $.get("http://127.0.0.1:5000/cambiaGabo", function(data, status){
+            $("#fotoGabo").attr("src", data);
+            $("#fotoGabo").attr("height", 225);
+            $("#fotoGabo").attr("width", 225);
+            alert("Acaba de cambiar la foto de Gabo. Status: " + status);
+        });
+    });
 
-function menus(dia){
-    if (dia === "lunes"){
-       console.log(`Mostrando menu del dia ${dia}`);
-       document.getElementById("infomenu").innerText="Sopa de letras, nachos con queso, carne al pastor, agua de limón y un cheesecake de frambuesa.";
-    }
-    if (dia === "martes"){
-       console.log(`Mostrando menu del dia ${dia}`);
-       document.getElementById("infomenu").innerText="Arroz rojo, tostadas preparadas, pechugas rellenas, agua de jamaica y gelatina de rompope.";
-    }
-    if (dia === "miercoles"){
-       console.log(`Mostrando menu del dia ${dia}`);
-       document.getElementById("infomenu").innerText="Consome de pollo, arroz blanco, enchiladas suizas, agua de horchata y arroz con leche.";
-    }
-    if (dia === "jueves"){
-       console.log(`Mostrando menu del dia ${dia}`);
-       document.getElementById("infomenu").innerText="Ensalada rusa, pozole rojo, picadillo de res, agua de fresa y pastel de chocolate.";
-    }
-    if (dia === "viernes"){
-       console.log(`Mostrando menu del dia ${dia}`);
-       document.getElementById("infomenu").innerText="Pasta boloñesa, pan de ajo, pizza de peperoni, sodas italianas y tiramisú.";
-    }
-    if (dia === "sabado"){
-       console.log(`Mostrando menu del dia ${dia}`);
-       document.getElementById("infomenu").innerText="Arroz blanco, camarones, tacos dorados, agua de rompope y pay de limon.";
-    }
-  }
+    $("#botonOmar").click(function(){
+        $.get("http://127.0.0.1:5000/cambiaOmar", function(data, status){
+            $("#fotoOmar").attr("src", data);
+            $("#fotoOmar").attr("height", 225);
+            $("#fotoOmar").attr("width", 225);
+            alert("Acaba de cambiar la foto de Omar. Status: " + status);
+        });
+    });
+    $("#botonLunes").click(function(){
+        $.get("http://127.0.0.1:5000/menuLunes", function(data, status){
+            $("#infomenu").text(data)
+            alert("Mostrando el menu del lunes. Status: " + status);
+        });
+    });
+    $("#botonMartes").click(function(){
+        $.get("http://127.0.0.1:5000/menuMartes", function(data, status){
+            $("#infomenu").text(data)
+            alert("Mostrando el menu del martes. Status: " + status);
+        });
+    });
+    $("#botonMiercoles").click(function(){
+        $.get("http://127.0.0.1:5000/menuMiercoles", function(data, status){
+            $("#infomenu").text(data)
+            alert("Mostrando el menu del miercoles. Status: " + status);
+        });
+    });
+    $("#botonJueves").click(function(){
+        $.get("http://127.0.0.1:5000/menuJueves", function(data, status){
+            $("#infomenu").text(data)
+            alert("Mostrando el menu del jueves. Status: " + status);
+        });
+    });
+    $("#botonViernes").click(function(){
+        $.get("http://127.0.0.1:5000/menuViernes", function(data, status){
+            $("#infomenu").text(data)
+            alert("Mostrando el menu del viernes. Status: " + status);
+        });
+    });
+    $("#botonSabado").click(function(){
+        $.get("http://127.0.0.1:5000/menuSabado", function(data, status){
+            $("#infomenu").text(data)
+            alert("Mostrando el menu del sabado. Status: " + status);
+        });
+    });
+});
 
   function gatitoAscii(){
     const gato = [
