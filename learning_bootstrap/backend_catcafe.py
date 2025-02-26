7e77ccc174a14ab833649061c2af9a7673ed93c0
Santiago Ramirez Niño <A01665906@tec.mx>
2025-02-25 22:59:45 -0600

    Optimizando catcafe

--- learning_bootstrap/backend_catcafe.py
+++ learning_bootstrap/backend_catcafe.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -16,40 +16,59 @@ def saludo():
                     <li>Hot cakes ----- $60</li>
                     <li>Sandwich ------ $40</li>
                     <li>Café ---------- $25</li>
-                </ol>"""
+            </ol>"""
 
-@app.route("/cambiaNacho")
-def nuevoNacho():
-    return "https://images.unsplash.com/photo-1606208427954-aa8319c4815e?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2F0byUyMGJsYW5jb3xlbnwwfHwwfHx8MA%3D%3D"
+@app.route("/cambiaImagen/<gato>")
+def nuevaImagen(gato):
+    imagenes = {
+        "nacho": "https://images.unsplash.com/photo-1606208427954-aa8319c4815e?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Z2F0byUyMGJsYW5jb3xlbnwwfHwwfHx8MA%3D%3D",
+        "gabo": "https://www.mirringo.com.co/Portals/mirringo/Images/articulos-actualidad-gatuna/5-mitos-sobre-los-gatos-naranjas/gatos-de-color-naranja.png?ver=2024-10-30-115216-760",
+        "omar": "https://www.hola.com/horizon/square/7a62013a79a9-por-que-gatos-negros-dan-mala-suerte-t.jpg"
+    }
+    if gato in imagenes:
+        return jsonify({"url": imagenes[gato]}),200
+    else:
+        return jsonify({"error": "No existe la imagen de ese gato"}),400
 
-@app.route("/cambiaGabo")
-def nuevoGabo():
-    return "https://www.mirringo.com.co/Portals/mirringo/Images/articulos-actualidad-gatuna/5-mitos-sobre-los-gatos-naranjas/gatos-de-color-naranja.png?ver=2024-10-30-115216-760"
 
-@app.route("/cambiaOmar")
-def nuevoOmar():
-    return "https://www.hola.com/horizon/square/7a62013a79a9-por-que-gatos-negros-dan-mala-suerte-t.jpg"
+@app.route("/menu/<dia>")
+def mostrarMenu(dia):
+    menus = {
+        "lunes": "Sopa de letras, nachos con queso, carne al pastor, agua de limón y un cheesecake de frambuesa.",
+        "martes": "Arroz rojo, tostadas preparadas, pechugas rellenas, agua de jamaica y gelatina de rompope.",
+        "miercoles": "Consome de pollo, arroz blanco, enchiladas suizas, agua de horchata y arroz con leche.",
+        "jueves": "Ensalada rusa, pozole rojo, picadillo de res, agua de fresa y pastel de chocolate.",
+        "viernes": "Pasta boloñesa, pan de ajo, pizza de peperoni, sodas italianas y tiramisú.",
+        "sabado": "Arroz blanco, camarones, tacos dorados, agua de rompope y pay de limon."
+    }
+    if dia in menus:
+        return jsonify({"platillos": menus[dia]}),200
+    else:
+        return ({"error": "No existe el menú de ese día"}),400
+    
+@app.route("/info/<gato>")
+def infoGatos(gato):
+    descripciones = {
+        "nacho": "El es Nacho, un gato blanco, muy amable y cariñoso. "
+                "Le encanta dormir en los lugares más cómodos de la casa. "
+                "Siempre busca compañía y se acurruca en el regazo de las personas. "
+                "Su comida favorita es el atún y odia los ruidos fuertes.",
 
-@app.route("/menuLunes")
-def menuMonday():
-    return "Sopa de letras, nachos con queso, carne al pastor, agua de limón y un cheesecake de frambuesa."
+        "gabo": "El es Gabriel, un gato naranja muy violento e inteligente. "
+                "Tiene una mirada desafiante y siempre está listo para la acción. "
+                "Le gusta trepar a los lugares más altos y observar todo desde arriba. "
+                "A pesar de su actitud ruda, disfruta de los mimos cuando nadie lo ve.",
 
-@app.route("/menuMartes")
-def menuTuesday():
-    return "Arroz rojo, tostadas preparadas, pechugas rellenas, agua de jamaica y gelatina de rompope."
+        "omar": "El es Omar, un gato negro muy tenebroso y kawai. "
+                "Se esconde en las sombras y aparece cuando menos lo esperas. "
+                "Su pasatiempo favorito es perseguir luces y jugar con sombras. "
+                "Aunque parece misterioso, en realidad es muy juguetón y curioso."
+    }
 
-@app.route("/menuMiercoles")
-def menuWednesday():
-    return "Consome de pollo, arroz blanco, enchiladas suizas, agua de horchata y arroz con leche."
+    if gato in descripciones:
+        return jsonify({"descripcion": descripciones[gato]}),200
+    else:
+        return({"error": "No existe información de ese gato."}),400
 
-@app.route("/menuJueves")
-def menuThursday():
-    return "Ensalada rusa, pozole rojo, picadillo de res, agua de fresa y pastel de chocolate."
-
-@app.route("/menuViernes")
-def menuFriday():
-    return "Pasta boloñesa, pan de ajo, pizza de peperoni, sodas italianas y tiramisú."
-
-@app.route("/menuSabado")
-def menuSaturday():
-    return "Arroz blanco, camarones, tacos dorados, agua de rompope y pay de limon."
+if __name__ == "__main__":
+    app.run(debug=True)
--- learning_bootstrap/index.js
+++ learning_bootstrap/index.js
@@ -1,43 +1,54 @@
 $(document).ready(function(){
-    $("#infoNacho").hide();
-    $("#infoGabo").hide();
-    $("#infoOmar").hide();
-    $("#listaMenu").hide();
-    $("#fotoNacho").dblclick(function(){    
-        $("#infoNacho").fadeIn(200);       
+
+    $("#fotoNacho").dblclick(function(){
+        $.get("http://127.0.0.1:5000/info/nacho", function(data){
+            $("#infoNacho").hide().text(data.descripcion).fadeIn(200);
+        });
     });
-    $("#fotoGabo").dblclick(function(){    
-        $("#infoGabo").fadeIn(200);       
+
+    $("#fotoGabo").dblclick(function(){
+        $.get("http://127.0.0.1:5000/info/gabo", function(data){
+            $("#infoGabo").hide().text(data.descripcion).fadeIn(200);
+        });
     });
-    $("#fotoOmar").dblclick(function(){    
-        $("#infoOmar").fadeIn(200);       
+
+    $("#fotoOmar").dblclick(function(){
+        $.get("http://127.0.0.1:5000/info/omar", function(data){
+            $("#infoOmar").hide().text(data.descripcion).fadeIn(200);
+        });
     });
+
     $("#maps").mouseleave(function(){
         $("#maps").hide();
     });
+
     // $("#botonSlide").click(function(){
     //     $("#listaMenu").slideDown();
     // });
+
     $("#botonSlide").click(function(){
         $.get("http://127.0.0.1:5000/hi", function(data, status){
             $("#resultadomenu").html(data);
             alert("Acaba de cargar el menu. Status: " + status);
         });
     });
+
     $("#hideMenu").click(function(){   //Ya no me deja volver a mostrar.
         $("#resultadomenu").hide();
-    })
+    });
+
     $("#botonNacho").click(function(){
-        $.get("http://127.0.0.1:5000/cambiaNacho", function(data, status){
-            $("#fotoNacho").attr("src", data);
+        $.get("http://127.0.0.1:5000/cambiaImagen/nacho", function(data, status){
+            $("#fotoNacho").attr("src", data.url);
             $("#fotoNacho").attr("height", 225);
             $("#fotoNacho").attr("width", 225);
             alert("Acaba de cambiar la foto de Nacho. Status: " + status);
         });
     });
+
     $("#botonGabo").click(function(){
-        $.get("http://127.0.0.1:5000/cambiaGabo", function(data, status){
-            $("#fotoGabo").attr("src", data);
+        $.get("http://127.0.0.1:5000/cambiaImagen/gabo", function(data, status){
+            $("#fotoGabo").attr("src", data.url);
             $("#fotoGabo").attr("height", 225);
             $("#fotoGabo").attr("width", 225);
             alert("Acaba de cambiar la foto de Gabo. Status: " + status);
@@ -45,52 +56,58 @@ $(document).ready(function(){
     });
 
     $("#botonOmar").click(function(){
-        $.get("http://127.0.0.1:5000/cambiaOmar", function(data, status){
-            $("#fotoOmar").attr("src", data);
+        $.get("http://127.0.0.1:5000/cambiaImagen/omar", function(data, status){
+            $("#fotoOmar").attr("src", data.url);
             $("#fotoOmar").attr("height", 225);
             $("#fotoOmar").attr("width", 225);
             alert("Acaba de cambiar la foto de Omar. Status: " + status);
         });
     });
+
     $("#botonLunes").click(function(){
-        $.get("http://127.0.0.1:5000/menuLunes", function(data, status){
-            $("#infomenu").text(data)
+        $.get("http://127.0.0.1:5000/menu/lunes", function(data, status){
+            $("#infomenu").text(data.platillos)
             alert("Mostrando el menu del lunes. Status: " + status);
         });
     });
+
     $("#botonMartes").click(function(){
-        $.get("http://127.0.0.1:5000/menuMartes", function(data, status){
-            $("#infomenu").text(data)
+        $.get("http://127.0.0.1:5000/menu/martes", function(data, status){
+            $("#infomenu").text(data.platillos)
             alert("Mostrando el menu del martes. Status: " + status);
         });
     });
+
     $("#botonMiercoles").click(function(){
-        $.get("http://127.0.0.1:5000/menuMiercoles", function(data, status){
-            $("#infomenu").text(data)
+        $.get("http://127.0.0.1:5000/menu/miercoles", function(data, status){
+            $("#infomenu").text(data.platillos)
             alert("Mostrando el menu del miercoles. Status: " + status);
         });
     });
+
     $("#botonJueves").click(function(){
-        $.get("http://127.0.0.1:5000/menuJueves", function(data, status){
-            $("#infomenu").text(data)
+        $.get("http://127.0.0.1:5000/menu/jueves", function(data, status){
+            $("#infomenu").text(data.platillos)
             alert("Mostrando el menu del jueves. Status: " + status);
         });
     });
+
     $("#botonViernes").click(function(){
-        $.get("http://127.0.0.1:5000/menuViernes", function(data, status){
-            $("#infomenu").text(data)
+        $.get("http://127.0.0.1:5000/menu/viernes", function(data, status){
+            $("#infomenu").text(data.platillos)
             alert("Mostrando el menu del viernes. Status: " + status);
         });
     });
+
     $("#botonSabado").click(function(){
-        $.get("http://127.0.0.1:5000/menuSabado", function(data, status){
-            $("#infomenu").text(data)
+        $.get("http://127.0.0.1:5000/menu/sabado", function(data, status){
+            $("#infomenu").text(data.platillos)
             alert("Mostrando el menu del sabado. Status: " + status);
         });
     });
 });
 
-  function gatitoAscii(){
+function gatitoAscii(){
     const gato = [
        [' ', '/', '\\', '_', '/', '\\', ' '],
        ['(', ' ', 'o', '.', 'o', ' ', ')'],
@@ -104,4 +121,4 @@ $(document).ready(function(){
        });
        console.log(linea);
    });
-  }
+}
